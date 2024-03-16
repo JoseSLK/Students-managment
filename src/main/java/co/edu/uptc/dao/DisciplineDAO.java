835415142d08e2d1867954d1c0623c92aa243967
JoseSLK <jose.salamanca05@uptc.edu.co>
2024-03-15 20:31:39 -0500

    Add methods for class (Discipline), add and find

--- src/main/java/co/edu/uptc/dao/DisciplineDAO.java
+++ src/main/java/co/edu/uptc/dao/DisciplineDAO.java
@@ -1,12 +1,45 @@
 package co.edu.uptc.dao;
 
+import co.edu.uptc.model.Discipline;
+import co.edu.uptc.model.Student;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisciplineDAO {
     private final MongoCollection<Document> disciplinesCollection;
 
     public DisciplineDAO() {
         this.disciplinesCollection = ConnectionBaseData.getInstance().getDisciplineCollection();
+    }
+
+    public Discipline addDiscipline(String name, int id){
+        Discipline discipline = new Discipline(name, id, new ArrayList<>());
+        Document doc = new Document("name", discipline.getName())
+                .append("_id", discipline.getId())
+                .append("participants", discipline.getParticipants());
+        disciplinesCollection.insertOne(doc);
+        return discipline;
+    }
+    public Discipline deleteDiscipline(){
+        return null;
+    }
+    public Discipline modifyDiscipline(){
+        return null;
+    }
+    public Discipline findDisciplineById(int idDiscipline){
+        Document query = new Document("_id", idDiscipline);
+        Document doc = disciplinesCollection.find(query).first();
+
+        if(doc != null){
+            String name = doc.getString("name");
+            int id = doc.getInteger("_id");
+            List<Integer> participantIds = (List<Integer>) doc.get("participants");
+            ArrayList<Integer> participants = new ArrayList<>(participantIds);
+
+            return new Discipline(name, id, participants);
+        }else return null;
     }
 }
--- src/main/java/co/edu/uptc/model/Discipline.java
+++ src/main/java/co/edu/uptc/model/Discipline.java
@@ -4,20 +4,24 @@ import java.util.ArrayList;
 
 public class Discipline {
     private String name;
-    private ArrayList<Student> participants;
+    private ArrayList<Integer> participants;
     private int id;
 
-    public Discipline(String name, int id) {
+    public Discipline(String name, int id, ArrayList<Integer> participants) {
         this.name = name;
+        this.participants = participants;
         this.id = id;
         this.participants = new ArrayList<>();
     }
 
-    public ArrayList<Student> getParticipants() {
+    public Discipline() {
+    }
+
+    public ArrayList<Integer> getParticipants() {
         return participants;
     }
 
-    public void setParticipants(ArrayList<Student> participants) {
+    public void setParticipants(ArrayList<Integer> participants) {
         this.participants = participants;
     }
 
