15af0838bda6b8ab8978a971b9f2630ada3c9fda
alejodiazzz <diaza.alejandro123@gmail.com>
2024-03-16 11:14:21 -0500

    Add CRUD(EventDAO)

--- src/main/java/co/edu/uptc/dao/EventDAO.java
+++ src/main/java/co/edu/uptc/dao/EventDAO.java
@@ -1,7 +1,13 @@
 package co.edu.uptc.dao;
 
+
+import co.edu.uptc.model.Event;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class EventDAO {
     private final MongoCollection<Document> eventsCollection;
@@ -9,4 +15,68 @@ public class EventDAO {
     public EventDAO() {
         this.eventsCollection = ConnectionBaseData.getInstance().getEventsCollection();
     }
+
+    public Event addEvent(int discipline, String location, Date date, String name, int id) {
+        Event event = new Event(discipline, location, date, name, id);
+        Document doc = new Document("discipline", event.getDiscipline())
+                .append("_id", event.getId())
+                .append("position", event.getPosition())
+                .append("location", event.getLocation())
+                .append("date", event.getDate())
+                .append("name", event.getName());
+        eventsCollection.insertOne(doc);
+        return event;
+    }
+
+    public Event deleteEvent(int eventId) {
+        Event eventToDelete = findEventById(eventId);
+        if (eventToDelete != null) {
+            Document query = new Document("_id", eventId);
+            eventsCollection.deleteOne(query);
+        }
+        return eventToDelete;
+    }
+
+    public Event modifyEvent(int eventId, String location, Date date, String name) {
+        Event event = findEventById(eventId);
+
+        if (event != null) {
+            event.setLocation(location);
+            event.setDate(date);
+            event.setName(name);
+
+            Document query = new Document("_id", eventId);
+            Document updateQuery = new Document("$set", new Document("location", location)
+                    .append("date", date)
+                    .append("name", name));
+            UpdateResult result = eventsCollection.updateOne(query, updateQuery);
+
+            if (result.getModifiedCount() > 0) {
+                return event;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public Event findEventById(int eventId) {
+        Document query = new Document("_id", eventId);
+        Document doc = eventsCollection.find(query).first();
+
+        if (doc != null) {
+            int discipline = doc.getInteger("discipline");
+            List<Integer> positionIds = (List<Integer>) doc.get("position");
+            ArrayList<Integer> position = new ArrayList<>(positionIds);
+            String location = doc.getString("location");
+            Date date = doc.getDate("date");
+            String name = doc.getString("name");
+
+            return new Event(discipline, location, date, name, eventId);
+        } else {
+            return null;
+        }
+    }
 }
+
